5894ecd85151629e56e811c71822f9f1646c6df2
Dreffy-0 <130404080+Dreffy-0@users.noreply.github.com>
2023-08-17 11:27:12 -0300

    Update exercicios

--- exercicios.c
+++ exercicios.c
@@ -1,1 +1,76 @@
 #include<stdio.h>
+
+main(){
+// Exercicio 1
+/* int num1, num2, soma, sub, mult, divi;
+
+printf("Digite o primeiro numero: ");
+scanf("%d", &num1);
+
+printf("\nDigite o segundo numero: ");
+scanf("%d", &num2);
+
+soma = num1 + num2;
+sub = num1 - num2;
+mult = num1 * num2;
+divi = num1 / num2;
+
+printf( "A soma e: %i\n", soma );
+printf( "A subtracao e: %i\n", sub );
+printf( "A multiplicacao e: %i \n", mult );
+printf( "A divisão e: %i \n", divi ); */
+
+//exercicio 2
+
+/* int num, dobro;
+printf("Digite um valor: ");
+scanf("%i", &num);
+
+dobro = num + num;
+printf("O dobro do valor informado e: %i", dobro); */
+
+//execicio 3
+
+/* float area, altura, largura; 
+printf("Informe a altura e a largura da sala, separe as medidas com espaco: ");
+scanf("%f%f", &altura, &largura);
+
+area = altura * largura;
+
+printf("A area da sala e: %.2f metros quadrados", area); */
+
+// exercicio 4
+
+/* float cotacao, dolar, real;
+
+printf("Informe a cotacao do dolar: ");
+scanf("%f", &cotacao);
+
+printf("Valor(R$): ");
+scanf("%f", &real);
+
+dolar = real / cotacao;
+
+printf("Quantia em dolar: US$ %.2f\n\n", dolar); */
+
+// exercicio 5
+
+/* float custo, venda, frete, despesas, lucro;
+
+printf("Digite o valor de venda da mercadoria: ");
+scanf("%f", &venda);
+
+printf("Digite o valor de custo, frete e despesas(Separe as informacoes com um espaco em branco): ");
+scanf("%f%f%f", &custo, &frete, &despesas);
+
+float valorBruto = custo + frete + despesas;
+lucro = (venda - valorBruto);
+int porcLucro = ( lucro / venda ) * 100;
+
+printf("A porcentagem de lucro e: %i%%", porcLucro); */
+
+
+
+
+
+}
