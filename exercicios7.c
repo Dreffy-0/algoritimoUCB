3a7e76d178b5b2e0812559137c23e4bf98b08945
Frederico Augusto <130404080+Dreffy-0@users.noreply.github.com>
2023-09-11 11:21:40 -0300

    Add files via upload

--- exercicios7.c
+++ exercicios7.c
@@ -36,16 +36,51 @@ int main()
     scanf("%d", &n); */
 
     // exercicio 4
-    int n;
-    printf("Digite um numero positivo e natural\n");
-    scanf("%d", &n);
-    for(int i = 0; i <= n; i++){
-        int cont = 1;
-        if(cont != 5 && n % 3 == 0){
-            cont++;
-            printf("\n%d", i);
+    /*     int i, mult;
+        i = 1;
+        mult = 0;
+        while(i > 0){
+            if(i % 3 == 0){
+                printf("\nO numero %d e um multiplo de 3", i);
+                mult++;
+            }
+            if(mult == 5){
+                break;
+            }
+            i++;
+        } */
+
+    // exercicio 5
+
+/*     int i, pares, soma;
+    i = 1;
+    pares = 0;
+    soma = 0;
+    while (i > 0)
+    {
+        if (i % 2 == 0)
+        {
+            printf("\nO numero %d e par", i);
+            soma += i;
+            pares++;
         }
-    }
-    
-    
+        if (pares == 50)
+        {
+            printf("\nA soma de todos os numeros pares e %d", soma);
+            break;
+        }
+        i++;
+    } */
+
+    // exercicio 6
+    int cod;
+    float total;
+    printf("-----------------------------------------------------------------------------");
+    printf("\n\t\tFrutas disponiveis:");
+    printf("\n\tCODIGO\t| \t-FRUTA-\t| \t -PRECO/unidade-");
+    printf("\n\t1\t| \tABACAXI\t| \t R$5,00");
+    printf("\n\t2\t| \tMACA\t| \t R$1,00");
+    printf("\n\t3\t| \tPERA\t| \t R$4,00");
+    printf("\n\t\tFIM CATALOGO");
+    printf("\n-----------------------------------------------------------------------------");
 }
